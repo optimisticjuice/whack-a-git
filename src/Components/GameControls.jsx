0dd3f734ed6c05890843ae4229bb3bb30ba38801
Justamanuelax <justamanuelax@gmail.com>
2025-05-06 22:09:09 +0200

    Setup the condition and matched handleStartGame and handlePauseGame to onClick

--- src/Components/GameControls.jsx
+++ src/Components/GameControls.jsx
@@ -1,1 +1,24 @@
 import "../Styles/GameControls.css";
+import { GameContext } from "../Context/GameContextDefinition";
+import { useContext } from "react";
+
+const GameControls = () => {
+    const {state, dispatch} = useContext(GameContext);
+    const {status, difficulty} = state;
+
+    const handleStartGame = () => {
+        if(status === "ended"){
+            dispatch({type: "RESET_GAME"});
+        }
+        dispatch({type: "START_GAME"});
+    };
+
+    return(
+        <div className="game-controls">
+         <div className="buttons">
+            <button onClick={handleStartGame} disabled={status === "playing" || status === "paused"}>{status === "ended" ? "Restart Game" : }
+         </div>        
+        </div>
+    )
+}
+
---
+++ src/Context/GameContext.jsx
@@ -0,0 +1,35 @@
+import { useReducer } from "react"; 
+import { initialState } from "./GameConstants";
+import { GameContext } from "./GameContextDefinition"; 
+
+function gameReducer(state, action){
+    const {gameDuration, status, timer, score, highScores, difficulty } = {state};
+    switch(action.type){
+        case "START_GAME":
+            return {...state, 
+                status: "playing",
+                timer: timer > 0 ? timer : gameDuration,
+                score: status === "paused" ? score : 0
+            };
+            // When the timer is 0 reset to gameDuration else make the timer decrement as intended
+        case "PAUSE_GAME":
+            return {...state, 
+                status: "paused"
+            }    
+            // When the game is paused, the timer is paused
+    }
+}
+
+
+
+export const GameProvider = ({children}) => {
+    const [state, dispatch] = useReducer(gameReducer, initialState);
+    // useReducer it from the state and dispatch.
+    
+    return (
+        // The GameContext.Provider ensures that the state and dispatch are available to all components in the children.
+        <GameContext.Provider value={{state, dispatch}}>
+            {children}
+        </GameContext.Provider>
+    );
+}
