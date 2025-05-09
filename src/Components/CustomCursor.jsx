78ca5372c2ded88d2aef90c1b784da830ed4c905
Justamanuelax <justamanuelax@gmail.com>
2025-05-09 21:09:43 +0200

    Cursor Redone - Added Styling and Functionality

--- src/Components/CustomCursor.jsx
+++ src/Components/CustomCursor.jsx
@@ -5,5 +5,32 @@ function CustomCursor(){
         <img src="/assets/images/stormbreaker.svg" className="cursor" alt="Custom_Cursor"/> 
     )
 }
+function CursorFollow(){
+    const cursor = document.querySelector(".cursor");
+    if(!cursor)return;
 
-export default CustomCursor;
+    const moveCursor = (e) => {
+        requestAnimationFrame(() => {
+            cursor.style.left = `${e.pageX}px`;
+            cursor.style.top = `${e.pageY}px`;
+        });
+    }
+    const handleMouseOver = (e) => {
+       if (e.target.tagName === "BUTTON") {
+        cursor.style.display = "none";
+        document.body.style.cursor = "pointer";
+    } else {
+        cursor.style.display = "block";
+        document.body.style.cursor = "none";
+       } 
+    }
+    document.addEventListener("mousemove", moveCursor);
+    document.addEventListener("mouseover", handleMouseOver);
+
+    return () => {
+        document.removeEventListener("mousemove", moveCursor);
+        document.removeEventListener("mouseover", handleMouseOver);
+    }
+}
+
+export {CustomCursor, CursorFollow};
